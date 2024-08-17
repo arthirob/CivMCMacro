b9f3c74f4d427ede3a93bc9232f7c45148870cf2
arthirob <perceval@debian.arthirob>
2024-08-17 22:42:22 +0200

    Treefarm building is working

--- Building/TreeFarm.js
+++ Building/TreeFarm.js
@@ -1,7 +1,7 @@
 //Script to build a layer of a tree farm
 
 const zNorth = 0;
-const zSouth = 30;
+const zSouth = 15;
 const xEast = -8;
 const xWest = -32;
 
@@ -11,7 +11,8 @@ const p = Player.getPlayer() ;
 const inv = Player.openInventory();
 const distOfDirt = 5;
 const distRow =5;
-var prevZ =p.getZ();
+var prevZ =Math.floor(p.getZ());
+var currentRow = Math.floor(p.getX());
 
 function lookAtCenter(x, z) {// Look at the center of a block
     p.lookAt(x+0.5,p.getY()+0.5, z+0.5);
@@ -19,28 +20,42 @@ function lookAtCenter(x, z) {// Look at the center of a block
 
 function walkTo(x, z) { // Walk to the center of a block
     lookAtCenter(x,z);
+    KeyBind.keyBind("key.sneak", true);
     KeyBind.keyBind("key.forward", true);
-    while ((Math.abs(p.getX() - x - 0.5) > 0.2 || Math.abs(p.getZ() - z - 0.5 ) > 0.2)){
+    while ((Math.abs(p.getX() - x - 0.5) > 0.01 || Math.abs(p.getZ() - z - 0.5 ) > 0.01)){
         lookAtCenter(x,z);//Correct the trajectory if needed
         Time.sleep(10);
     }
     KeyBind.keyBind("key.forward", false);
     Client.waitTick(3);
-    
+}
+
+function alignX(x){
+    var currentX = p.getX()
+    if ((Math.abs(currentX-x-0.5))>0.06) { //You are not aligned
+        p.lookAt(180,0);
+        if (currentX>(x+0.5)) {
+            KeyBind.keyBind("key.left", true);
+            Client.waitTick();
+            KeyBind.keyBind("key.left", false);
+            alignX(x)
+        } else {
+            KeyBind.keyBind("key.right", true);
+            Client.waitTick();
+            KeyBind.keyBind("key.right", false);
+            alignX(x);
+        }
+    }
 }
 
 function placeFill(i,item) { //Autofill the i slot
     inv.setSelectedHotbarSlotIndex(i);
-    Chat.log(item);
     p.interact();
     Client.waitTick();
     if (inv.getSlot(36+i).getItemId()=="minecraft:air") { //i slot empty
         list = inv.findItem(item);
-        Chat.log("length"+list.length);
-        Chat.log("firstvalue"+list[0]);
         if (list.length==0) {
             KeyBind.keyBind("key.back", false);
-            KeyBind.keyBind("key.left", false);
             KeyBind.keyBind("key.forward", true);
             Client.waitTick(3);
             KeyBind.keyBind("key.forward", false);
@@ -54,13 +69,14 @@ function placeFill(i,item) { //Autofill the i slot
 }
 
 
-function bridgeLine(item,dir){
+function bridgeLine(dir){
+    var finished = false
     var placedBlock=0 ;
     p.lookAt(dir,80);
     KeyBind.keyBind("key.sneak", true);
     KeyBind.keyBind("key.back", true);
     prevZ = p.getZ();
-    while ((zNorth < prevZ)&&(prevZ < zSouth)){
+    while (!finished){
         prevZ = p.getZ();
         Client.waitTick();
         if (prevZ==p.getZ()) {
@@ -72,30 +88,36 @@ function bridgeLine(item,dir){
                 placedBlock+=1;
             }
         }
+        if (dir==0) {
+            if (prevZ < zNorth) {
+                finished = true;
+            }
+        } else {
+            if (prevZ > zSouth) {
+                finished = true;
+            }
+        }
         
     }
-    KeyBind.keyBind("key.sneak", false);
     KeyBind.keyBind("key.back", false);
-    KeyBind.keyBind("key.forward", true);
-    Client.waitTick(3);
-    KeyBind.keyBind("key.forward", false);
 }
 
 function bridgeFloor(){
-    var dir = (Math.floor((p.getYaw()+90)/180))*180;
-    var item = inv.getSlot(36);
-    while ((xWest <= p.getX())&&(p.getX() <= xEast)){
-        bridgeLine(item,dir);
-        Chat.log(Math.floor(p.getX()+distRow));
-        if (dir==180) {
+    var dir = 180;
+    walkTo(currentRow,prevZ);
+    alignX(currentRow);
+    while (currentRow+distRow<xEast){
+        bridgeLine(dir);
+        if (dir==0) {
             zObj = zNorth ;
         } else {
             zObj = zSouth ;
         }
-        walkTo(Math.floor(p.getX()+distRow),zObj);
+        currentRow+=distRow;
+        walkTo(currentRow,zObj);
+        alignX(currentRow);
         dir = (dir+180)%360;
     }
 }
 
-Chat.log(inv.findItem("minecraft:glass_pane")[0])
 bridgeFloor();
