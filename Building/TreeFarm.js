31db1bed1c20235066e0bac7d21b917841692198
arthirob <perceval@debian.arthirob>
2024-08-17 22:04:19 +0200

    Fixed the treefarm

--- Building/TreeFarm.js
+++ Building/TreeFarm.js
@@ -1,25 +1,27 @@
 //Script to build a layer of a tree farm
 
-const zNorth = -6279;
-const zSouth = -6124;
-const xEast = 6376;
-const xWest = 6326;
+const zNorth = 0;
+const zSouth = 30;
+const xEast = -8;
+const xWest = -32;
 
-
+const fullBock = "minecraft:dirt";
+const slimBlock = "minecraft:glass_pane";
 const p = Player.getPlayer() ;
 const inv = Player.openInventory();
-const distOfDirt = 7;
-const distRow =7;
+const distOfDirt = 5;
+const distRow =5;
 var prevZ =p.getZ();
 
 function lookAtCenter(x, z) {// Look at the center of a block
-    p.lookAt(x+0.5,p.getY()+1.5, z+0.5);
+    p.lookAt(x+0.5,p.getY()+0.5, z+0.5);
 }
 
 function walkTo(x, z) { // Walk to the center of a block
     lookAtCenter(x,z);
     KeyBind.keyBind("key.forward", true);
-    while ((Math.abs(p.getX() - x - 0.5) > 0.4 || Math.abs(p.getZ() - z - 0.5 ) > 0.4)){
+    while ((Math.abs(p.getX() - x - 0.5) > 0.2 || Math.abs(p.getZ() - z - 0.5 ) > 0.2)){
+        lookAtCenter(x,z);//Correct the trajectory if needed
         Time.sleep(10);
     }
     KeyBind.keyBind("key.forward", false);
@@ -27,23 +29,28 @@ function walkTo(x, z) { // Walk to the center of a block
     
 }
 
-function placeFill(item) {
+function placeFill(i,item) { //Autofill the i slot
+    inv.setSelectedHotbarSlotIndex(i);
+    Chat.log(item);
     p.interact();
-            if (inv.findFreeHotbarSlot()==36) { //First slot empty
-                list = inv.findItem(item);
-                Chat.log("Listlength"+list.length);
-                if (list.length==0) {
-                    KeyBind.keyBind("key.sneak", false);
-                    KeyBind.keyBind("key.back", false);
-                    KeyBind.keyBind("key.forward", true);
-                    Client.waitTick(3);
-                    KeyBind.keyBind("key.forward", false);
-                    Chat.log("Out of materials")
-                    throw("No more mats")
-                }
-                inv.quick(list[0]);
-                Client.waitTick(10);
-            }
+    Client.waitTick();
+    if (inv.getSlot(36+i).getItemId()=="minecraft:air") { //i slot empty
+        list = inv.findItem(item);
+        Chat.log("length"+list.length);
+        Chat.log("firstvalue"+list[0]);
+        if (list.length==0) {
+            KeyBind.keyBind("key.back", false);
+            KeyBind.keyBind("key.left", false);
+            KeyBind.keyBind("key.forward", true);
+            Client.waitTick(3);
+            KeyBind.keyBind("key.forward", false);
+            KeyBind.keyBind("key.sneak", false);
+            Chat.log("Out of materials")
+            throw("No more mats")
+        }
+        inv.swapHotbar(list[0],i);
+        Client.waitTick();
+    }
 }
 
 
@@ -58,22 +65,15 @@ function bridgeLine(item,dir){
         Client.waitTick();
         if (prevZ==p.getZ()) {
             if (placedBlock==(distOfDirt-1)) {
-                KeyBind.keyBind("key.hotbar.2", true);
-                Client.waitTick();
-                KeyBind.keyBind("key.hotbar.2", false);
-                p.interact();
+                placeFill(1,fullBock);
                 placedBlock = 0;
-                KeyBind.keyBind("key.hotbar.1", true);
-                Client.waitTick();
-                KeyBind.keyBind("key.hotbar.1", false);
             } else {
-                placeFill(item);
+                placeFill(0,slimBlock);
                 placedBlock+=1;
             }
         }
         
     }
-
     KeyBind.keyBind("key.sneak", false);
     KeyBind.keyBind("key.back", false);
     KeyBind.keyBind("key.forward", true);
@@ -87,9 +87,15 @@ function bridgeFloor(){
     while ((xWest <= p.getX())&&(p.getX() <= xEast)){
         bridgeLine(item,dir);
         Chat.log(Math.floor(p.getX()+distRow));
-        walkTo(Math.floor(p.getX()+distRow),Math.floor(p.getZ()));
+        if (dir==180) {
+            zObj = zNorth ;
+        } else {
+            zObj = zSouth ;
+        }
+        walkTo(Math.floor(p.getX()+distRow),zObj);
         dir = (dir+180)%360;
     }
 }
 
+Chat.log(inv.findItem("minecraft:glass_pane")[0])
 bridgeFloor();
