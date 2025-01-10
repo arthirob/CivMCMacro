3cd3b4a7dec1b7b2bb7a76c9bd007825c902b9a6
arthirob <arthirob@hotmail.fr>
2025-01-10 02:29:17 +0100

    Added autoswitch

---
+++ Utility/AutoSwitch.js
@@ -0,0 +1,8 @@
+const inv = Player.openInventory();
+
+if (Chat.getHistory().getRecvLine(0).getText().getString() == "A SimpleAdminHacks /config option is preventing you from breaking that ore without a silk touch pickaxe.") { // You hav ctb activating
+    inv.setSelectedHotbarSlotIndex(1);
+    Client.waitTick(16);
+    inv.setSelectedHotbarSlotIndex(0);
+
+}
