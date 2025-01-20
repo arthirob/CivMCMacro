42becb7be7019ce1de987f66b57dbf2eee751e55
arthirob <arthirob@hotmail.fr>
2025-01-20 06:08:57 +0100

    Made the lodestone macro more clear

--- Utility/ChangeLodeStone.js
+++ Utility/ChangeLodeStone.js
@@ -1,4 +1,4 @@
-const lagTick = 6;
+const lagTick = 5;
 Chat.say("/config");
 Client.waitTick(lagTick);
 var inv = Player.openInventory();
@@ -7,4 +7,11 @@ Client.waitTick(lagTick);
 inv = Player.openInventory();
 inv.click(14,0);
 Client.waitTick(lagTick);
+if (inv.getSlot(14).getItemID()=="minecraft:red_dye") {
+    Chat.log("Lodestone config is now jumping")
+} else if ((inv.getSlot(14).getItemID()=="minecraft:lime_dye")){
+    Chat.log("Lodestone config is now hitting the lodestone")
+} else {
+    Chat.log("Couldn't change the config")
+}
 inv.close();
