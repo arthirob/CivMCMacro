3607236174f3d2c358e9c4856d7535d582ba58b0
Jim McCall <git@jpmiii.com>
2025-07-25 15:13:43 -0500

    HUD, reverse, variable waypoint settings and labels

--- boating/boat.js
+++ boating/boat.js
@@ -1,24 +1,44 @@
-//boating by jpmiii
+// boating by jpmiii
 
 
 var waypoints = [
-{ x: 0, z: -0},
-{ x: 100, z: 100}
+{ x: 0, z: -0 , slow: 15, target: 3 , label: "home"},
+{ x: 123, z: -123, slow: 15, target: 3 },
+{ x: 345, z: -345 }, // there are defaults for slow, target, and label defaults to index+1
+{ x: 3, z: -4 , slow: 0, target: 15 , label: "almost there"},
+{ x: 99, z: -99 , slow: 15, target: 5 , label: "woo hoo"},
 ];
 
+var reverse = false; //set to true to boat in reverse  order
 
-
-
+const d2d = Hud.createDraw2D();
+let dismeter = null;
+d2d.setOnInit(JavaWrapper.methodToJava(() => {
+    dismeter = d2d.addText('Start Boating', 0, d2d.getHeight()-20 - 10, 0xFFFFFF, true);
+}));
+d2d.register();
 
 var boating = true;
 const p = Player.getPlayer();
-var waypoint_index = 0;
+const default_target = 5;
+const default_slow = 20
 var loop_count = 0;
 var stuck = 0;
-
 GlobalVars.putBoolean("stop",false);
 
 
+if (GlobalVars.getInt("boat_index")) {
+    waypoint_index = GlobalVars.getInt("boat_index");
+    //Chat.log(`resuming at waypoint ${waypoint_index+1}`);       
+} else {
+    if (reverse == true) {
+        waypoint_index = waypoints.length - 1
+    } else {
+        waypoint_index = 0;
+    }
+
+    //Chat.log(`starting at waypoint ${waypoint_index+1}`);       
+}
 
 function getUnitVector(v) {
   const length = Math.sqrt(v.x * v.x + v.z * v.z);
@@ -47,33 +67,48 @@ function signedAngleBetweenVectors(v1, v2) {
     return angleRad * Math.sign(crossProduct);
 }
 
-
+function finished() {
+    boating = false;
+    KeyBind.keyBind("key.forward", false);
+    KeyBind.keyBind("key.left", false);
+    KeyBind.keyBind("key.right", false);
+    GlobalVars.remove("boat_index");
+    World.playSound("entity.experience_orb.pickup", 100);
+    Chat.log("finished boating");
+    d2d.unregister();    
+}
 
 while (boating) {
     loop_count++;
     if (GlobalVars.getBoolean("stop") == true) { 
-        Chat.log(`global: ${GlobalVars.getBoolean("stop")}`);
-            KeyBind.keyBind("key.left", false);
-            KeyBind.keyBind("key.right", false);
-            KeyBind.keyBind("key.forward", false);
+        Chat.log(`global stop: ${GlobalVars.getBoolean("stop")}`);
+        KeyBind.keyBind("key.left", false);
+        KeyBind.keyBind("key.right", false);
+        KeyBind.keyBind("key.forward", false);
+        d2d.unregister();
         break;        
         }
     const prevX = p.getX();
     const prevZ = p.getZ();
     const waypoint_distance = Math.sqrt((prevX - waypoints[waypoint_index].x) ** 2 + (prevZ - waypoints[waypoint_index].z) ** 2);
-    if (waypoint_distance > 50) {
+    if ('slow' in waypoints[waypoint_index]) {
+        var slow = waypoints[waypoint_index].slow;
+    } else {
+        var slow = default_slow;
+    }
+    if (waypoint_distance > slow) {
         KeyBind.keyBind("key.forward", true);
-        Client.waitTick(30);
+        Client.waitTick(20);
     } else {
         KeyBind.keyBind("key.forward", true);
-        Client.waitTick(4);
+        Client.waitTick(3);
         KeyBind.keyBind("key.forward", false);
-        Client.waitTick(5);
+        Client.waitTick(6);
         KeyBind.keyBind("key.forward", true);
-        Client.waitTick(4);
-        KeyBind.keyBind("key.forward", false);
-        Client.waitTick(5);
-        KeyBind.keyBind("key.forward", true);
+        // Client.waitTick(4);
+        // KeyBind.keyBind("key.forward", false);
+        // Client.waitTick(5);
+        // KeyBind.keyBind("key.forward", true);
     }
     
     const currentX = p.getX();
@@ -85,51 +120,81 @@ while (boating) {
         const waypoint_direction = getUnitVector({ x: currentX - waypoints[waypoint_index].x, z: currentZ - waypoints[waypoint_index].z });
         const boat_direction = getUnitVector({ x: currentX - prevX, z: currentZ - prevZ });
         const waypoint_distance = Math.sqrt((currentX - waypoints[waypoint_index].x) ** 2 + (currentZ - waypoints[waypoint_index].z) ** 2);
-
-        if (waypoint_distance < 5 ) {
-            waypoint_index++;
-            if (waypoint_index >= waypoints.length) {
-                boating = false;
-                KeyBind.keyBind("key.forward", false);
-                KeyBind.keyBind("key.left", false);
-                KeyBind.keyBind("key.right", false);
-                World.playSound("entity.experience_orb.pickup", 100);
-                Chat.log("finished boating");
-                break;
+        if ('target' in waypoints[waypoint_index]) {
+            var target = waypoints[waypoint_index].target;
+        } else {
+            var target = default_target;
+        }
+        if (waypoint_distance < target ) {
+            // if ('label' in waypoints[waypoint_index]) {
+            //     Chat.log(`waypoint ${waypoints[waypoint_index].label} reached`);
+            // } else {
+            //     Chat.log(`waypoint ${waypoint_index+1} reached`);
+            // }
+            if(reverse == false) {
+                waypoint_index++;
+            } else {
+                waypoint_index--;
             }
-            Chat.log(`waypoint ${waypoint_index} reached`);
+            GlobalVars.putInt("boat_index", waypoint_index);
+            if (reverse == true) {
+                if (waypoint_index < 0) {
+                    finished();    
+                    
+                    break;
+                }
+            } else {
+                if (waypoint_index >= waypoints.length) {
+                    finished();    
+                    break;
+                    // reverse = true;
+                    // World.playSound("entity.experience_orb.pickup", 100);
+                    // waypoint_index = waypoints.length - 1;
+                    // Client.waitTick(100);
+                }
+            }
+            
+
+            
             World.playSound("ui.toast.out", 100, .2);
         }
+    
 
 
         
         
         const angle = signedAngleBetweenVectors(boat_direction, waypoint_direction);
 
-        if (loop_count%10 == 0) {
-            Chat.log(`waypoint: ${waypoint_index+1} distance: ${Math.trunc(waypoint_distance)} `);
+        // if (loop_count%10 == 0) {
+        //     Chat.log(`waypoint: ${waypoint_index+1} distance: ${Math.trunc(waypoint_distance)} `);
+        // }
+        if ('label' in waypoints[waypoint_index]) {
+            dismeter?.setText(`waypoint: ${waypoints[waypoint_index].label} distance: ${Math.trunc(waypoint_distance)} `)
+        } else {
+            dismeter?.setText(`waypoint: ${waypoint_index+1} distance: ${Math.trunc(waypoint_distance)} `)
         }
+
         //Chat.log(`angle: ${angle}`);
         if (angle > 0 && angle <= 2.50) {
             KeyBind.keyBind("key.left", true);
             Client.waitTick(4);
             KeyBind.keyBind("key.left", false);
-            KeyBind.keyBind("key.right", false);
+            
         } else if (angle < 3.10 && angle > 2.50) {
             KeyBind.keyBind("key.left", true);
             Client.waitTick(1);
             KeyBind.keyBind("key.left", false);
-            KeyBind.keyBind("key.right", false);
+            
         } else if (angle < 0 && angle > -2.50) {
             KeyBind.keyBind("key.right", true);
             Client.waitTick(4);
             KeyBind.keyBind("key.right", false);
-            KeyBind.keyBind("key.left", false);
+            
         } else if (angle > -3.10 && angle <= -2.50) {
             KeyBind.keyBind("key.right", true);
             Client.waitTick(1);
             KeyBind.keyBind("key.right", false);
-            KeyBind.keyBind("key.left", false);
+            
         } else {
             KeyBind.keyBind("key.left", false);
             KeyBind.keyBind("key.right", false);
@@ -144,15 +209,15 @@ while (boating) {
             KeyBind.keyBind("key.right", false);
             KeyBind.keyBind("key.forward", false);
             break;
-        } else if (stuck > 10) {
+        } else if (stuck > 3) {
             Chat.log("boat appears to be stuck, trying to unstick");
             KeyBind.keyBind("key.left", true);
+            KeyBind.keyBind("key.back", true);
             Client.waitTick(10);
             KeyBind.keyBind("key.left", false);
             KeyBind.keyBind("key.right", false);
+            KeyBind.keyBind("key.back", false);
             KeyBind.keyBind("key.forward", true);
-            Client.waitTick(10);
         }
     }
-
 }
